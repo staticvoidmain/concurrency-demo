af0ba54d6dd453d7fed063fda446c8eb63a8a755
Ross Jennings <themissinglinq@gmail.com>
2013-07-22 23:48:41 -0500

    #scusaConf in a few days, ahhh!

---
+++ demo/part-1/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace part_1
+{
+	public class Helpers
+	{
+		private static readonly long[] cache = new long[256];
+
+		/// <summary>
+		/// We're not memoizing because this is meant to take some time.
+		/// </summary>
+		/// <param name="n"></param>
+		/// <returns></returns>
+		private static long Fib(int n, bool useCache = false)
+		{
+			if (n < 2) { return n; }
+
+			if (useCache)
+			{
+				long cached = FromCache(n);
+
+				if (cached != 0L) { return cached; }
+			}
+
+			long val = Fib(n - 1) + Fib(n - 2);
+
+			if (useCache)
+			{
+				if (cache.Length > n)
+				{
+					cache[n] = val;
+				}
+			}
+
+			return val;
+		}
+
+		private static long FromCache(int n)
+		{
+			long cached = 0L;
+			
+			if (cache.Length > n)
+			{
+				cached = cache[n];
+			}
+
+			return cached;
+		}
+
+		public static long[] FibonacciNumbers(int count)
+		{
+			var fibs = new long[count];
+
+			for (int i = 0; i < count; i++)
+			{
+				fibs[i] = Fib(i);
+			}
+
+			return fibs;
+		}
+
+		internal static BigInteger Sum(long[] seq)
+		{
+			BigInteger sum = BigInteger.Zero;
+
+			foreach (long item in seq)
+			{
+				sum += item;
+			}
+
+			return sum;
+		}
+	}
+
+	/// <summary>
+	/// Lets learn about tasks.
+	/// </summary>
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			HelloWorld();
+			// TaskCancellation();
+			// ContinueWith();
+			// FunkyClosures();
+
+			Console.WriteLine("the end");
+			Console.Read();
+		}
+
+		#region Step 1
+
+		/// <summary>
+		/// Simple introduction to the task api.
+		/// </summary>
+		private static void HelloWorld()
+		{
+			Task t = Task.Factory.StartNew(() =>
+			{
+				// just to make sure I don't look silly.
+				Thread.Sleep(50);
+				Console.WriteLine("Hello from another thread!");
+			});
+
+			Console.WriteLine("Hello from the main thread!");
+
+			Console.ReadLine();
+		}
+
+		#endregion
+
+		#region Step 2
+
+		private static void TaskCancellation()
+		{
+			var source = new CancellationTokenSource();
+			var token = source.Token;
+			var tasks = new Task[5];
+
+			Action<object> doStuff = (i) =>
+			{
+				int id = (int)i;
+				int timeout = 50 + (50 * id);
+
+				while (true)
+				{
+					if (token.IsCancellationRequested)
+					{
+						Console.Write("\r\n{0} cancelled!", id);
+						token.ThrowIfCancellationRequested();
+					}
+
+					Console.Write(id);
+					Thread.Sleep(timeout);
+				}
+			};
+
+			for (int i = 0; i < 5; i++)
+			{
+				// reminder: funky closures
+				tasks[i] = Task.Factory.StartNew(doStuff, i, token);
+			}
+
+			source.CancelAfter(2000);
+
+			Console.Read();
+		}
+
+		#endregion
+
+		#region Step 3
+
+		private static void ContinueWith()
+		{
+			const int numberOfDesiredFibs = 50;
+
+			// we can chain tasks together to do more interesting stuff.
+			var task = Task.Factory.StartNew(() =>
+			{
+				return Helpers.FibonacciNumbers(numberOfDesiredFibs);
+			})
+			.ContinueWith(fibs =>
+			{
+				Console.WriteLine("Sum of first {0} fibonacci numbers = {1}", numberOfDesiredFibs, Helpers.Sum(fibs.Result));
+			});
+
+			// now we can do anything else! if only I were more creative...
+			while (!task.IsCompleted)
+			{
+				Console.WriteLine("Still waiting...");
+				Thread.Sleep(500);
+			}
+
+			Console.WriteLine("Finally!");
+		}
+
+		#endregion
+
+		#region BONUS: Funky Closures
+
+		private static void FunkyClosures()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				Task.Factory.StartNew(() =>
+				{
+					Console.WriteLine(i);
+				});
+			}
+
+			Thread.Sleep(500);
+		}
+
+		#endregion
+	}
+}
---
+++ demo/part-2/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace part_2
+{
+	public class ConcurrencyProblems
+	{
+		static void Main(string[] args)
+		{
+			DataRaces();
+			// Deadlocks();
+
+			Console.WriteLine("the end");
+			Console.Read();
+		}
+
+		#region Data Races
+
+		private static int value = 0;
+
+		// who moved my cheese?
+		private static void DataRaces()
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				Setup();
+
+				Parallel.For(1, 10001, (_) =>
+				{
+					IncrementValue();
+				});
+
+				Console.WriteLine("The answer is... {0}", ConcurrencyProblems.value);
+				Console.WriteLine(ConcurrencyProblems.value != 10000 ? "Wait what?" : "We got lucky this time!");
+
+				Reset();
+			}
+		}
+
+		private static void Setup()
+		{
+			Console.WriteLine("Press any key to continue...");
+			Console.ReadLine();
+			Console.WriteLine("Harmless looking parallel loop...");
+		}
+
+		private static void Reset()
+		{
+			ConcurrencyProblems.value = 0;
+			Thread.Sleep(50);
+		}
+
+		private static void IncrementValue()
+		{
+			value += 1;
+		}
+
+		#endregion
+
+		#region Deadlocks
+
+		private static object lockOne = new object();
+		private static object lockTwo = new object();
+
+		private static void Deadlocks()
+		{
+			ManualResetEvent mre = new ManualResetEvent(false);
+
+			var task1 = Task.Factory.StartNew(() =>
+			{
+				mre.WaitOne();
+
+				lock (lockTwo)
+				{
+					Console.WriteLine("Task-1 entered lock#2");
+
+					Thread.Sleep(10);
+
+					lock (lockOne)
+					{
+						Console.WriteLine("Task-1 entered lock#1!");
+					}
+				}
+			});
+
+			var task2 = Task.Factory.StartNew(() =>
+			{
+				mre.WaitOne();
+
+				lock (lockOne)
+				{
+					Console.WriteLine("Task-2 entered lock#1");
+
+					Thread.Sleep(10);
+
+					lock (lockTwo)
+					{
+						Console.WriteLine("Task-2 entered lock#2!");
+					}
+				}
+			});
+
+			mre.Set();
+
+			Task.WaitAll(task1, task2);
+		}
+
+		#endregion
+	}
+}
